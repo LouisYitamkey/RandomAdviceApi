69e74d60f0e02ce39604aa127bec599b35cdbdc3
Kwaku Louis <bonglamgh@gmail.com>
2024-01-29 14:22:22 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Advice from './pages/advice'
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div className="bg-slate-50 h-screen flex justify-center items-center">
+        <Advice />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/button.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+interface ButtonProps {
+  label: string
+  onClick?: () => void
+}
+
+const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+  return (
+    <div className="flex justify-center items-center">
+      <button
+        className=" text-white my-10 px-10 py-4 rounded-md bg-sky-500"
+        onClick={onClick}
+      >
+        {label}
+      </button>
+    </div>
+  )
+}
+
+export default Button
